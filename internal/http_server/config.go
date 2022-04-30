b328d52a3638ee62b8ed635a45171b6c57b51e70
ipoluyanov <ipoluyanov@gmail.com>
2022-04-30 17:47:06 +0200

    config

--- internal/http_server/config.go
+++ internal/http_server/config.go
@@ -3,7 +3,7 @@ package http_server
 import "time"
 
 type Config struct {
-	UsingProxy               bool
-	PurgeInterval            time.Duration
-	MaxRequestsPerIPInSecond uint64
+	UsingProxy               bool          `json:"using_proxy"`
+	PurgeInterval            time.Duration `json:"purge_interval"`
+	MaxRequestsPerIPInSecond uint64        `json:"max_requests_per_ip_in_second"`
 }
