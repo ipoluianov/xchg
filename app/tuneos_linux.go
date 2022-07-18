957039081aa183a0fb1a0ce5e829b3c55683f2fe
ivan.poluianov <ivan.poluianov@gmail.com>
2022-07-18 12:10:53 +0300

    Update tuneos_linux.go

--- app/tuneos_linux.go
+++ app/tuneos_linux.go
@@ -23,11 +23,11 @@ func TuneFDs() {
 	if err != nil {
 		logger.Println("TuneFDs Getrlimit2 error: ", err)
 	}
-	var rLimit syscall.Rlimit
-	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+	var rLimit2 syscall.Rlimit
+	err = syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit2)
 	if err != nil {
 		logger.Println("TuneFDs Getrlimit2 error: ", err)
 	}
-	logger.Println("New limits:", rLimit)
+	logger.Println("New limits:", rLimit2)
 	logger.Println("TimeFDs end")
 }
