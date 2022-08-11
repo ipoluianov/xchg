a71ab479d56c83f255a426162b2d825ee8ee2329
ivan.poluianov <ivan.poluianov@gmail.com>
2022-08-11 21:06:31 +0300

    Create network_analyzer.go

---
+++ xchg_router/network_analyzer.go
@@ -0,0 +1,15 @@
+package xchg_router
+
+type NetworkAnalizer struct {
+}
+
+func NewNetworkAnalyzer() *NetworkAnalizer {
+	var c NetworkAnalizer
+	return &c
+}
+
+func (c *NetworkAnalizer) Start() {
+}
+
+func (c *NetworkAnalizer) Stop() {
+}
