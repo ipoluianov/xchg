63c0cd421e9560e2881cbcd099b98b354942f683
ivan.poluianov <ivan.poluianov@gmail.com>
2022-08-13 16:20:24 +0300

    Update connection.go

--- xchg/connection.go
+++ xchg/connection.go
@@ -3,7 +3,6 @@ package xchg
 import (
 	"encoding/binary"
 	"errors"
-	"fmt"
 	"net"
 	"sync"
 	"sync/atomic"
@@ -205,17 +204,17 @@ func (c *Connection) thReceive() {
 
 	for !c.stopping {
 		if c.conn == nil {
-			fmt.Println("connecting to", c.host)
+			//fmt.Println("connecting to", c.host)
 			c.conn, err = net.Dial("tcp", c.host)
 			if err != nil {
 				time.Sleep(100 * time.Millisecond)
-				fmt.Println("connecting to", c.host, " timeout")
+				//fmt.Println("connecting to", c.host, " timeout")
 				continue
 			}
 			incomingData = make([]byte, c.configMaxFrameSize)
 			incomingDataOffset = 0
 			c.callProcessorConnected()
-			fmt.Println("connecting to", c.host, " OK")
+			//fmt.Println("connecting to", c.host, " OK")
 		}
 
 		n, err = c.conn.Read(incomingData[incomingDataOffset:])
