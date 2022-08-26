c6a6f399790a7ee7fa74fa519fb2bd2205915e2f
ivan.poluianov <ivan.poluianov@gmail.com>
2022-08-26 20:11:04 +0300

    Update host.go

--- xchg_network/host.go
+++ xchg_network/host.go
@@ -1,10 +1,12 @@
 package xchg_network
 
 type host struct {
-	Address   string  `json:"address"`
-	Name      string  `json:"name"`
-	PublicKey string  `json:"pk"`
-	Score     float64 `json:"sc"`
+	Address    string  `json:"address"`
+	Name       string  `json:"name"`
+	PublicKey  string  `json:"pk"`
+	Score      float64 `json:"sc"`
+	EthAddress string  `json:"eth_addr"`
+	SolAddress string  `json:"sol_addr"`
 
 	online bool
 }
@@ -13,6 +15,8 @@ func NewHost(address string) *host {
 	var c host
 	c.Address = address
 	c.online = true
+	c.EthAddress = "0x60087f17b9dAF691DEd6c40Cb1fA6CE4407fa58C"
+	c.SolAddress = "todo"
 	c.recalcScore()
 	return &c
 }
